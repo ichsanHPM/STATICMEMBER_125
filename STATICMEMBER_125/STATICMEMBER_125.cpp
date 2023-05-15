b8b47b1b3768cf6d1afc0347c2bdf88c7c2808d2
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-05-15 17:24:58 +0700

    membuat hak akses kelas mahasiswa

--- STATICMEMBER_125/STATICMEMBER_125.cpp
+++ STATICMEMBER_125/STATICMEMBER_125.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 class mahasiswa {
+public:
+    static int nim;
+    int id;
+    string nama;
 
+    void setID();
+    void printALL();
+    mahasiswa(string pnama) :nama(pnama) { setID();}
 };
 
 
