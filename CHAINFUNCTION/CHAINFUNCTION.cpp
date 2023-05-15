e1b0da60f417616cdba9b0b4be6c21c03e70b328
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-05-15 18:56:45 +0700

    menghapus coment chaint function pada baris 11

--- CHAINFUNCTION/CHAINFUNCTION.cpp
+++ CHAINFUNCTION/CHAINFUNCTION.cpp
@@ -8,7 +8,7 @@ public:
     buku setjudul(string judul)
     {
         this->judul = judul;
-        return *this;                   //chain function
+        return *this;                   
     }
     string getjudul()
     {
