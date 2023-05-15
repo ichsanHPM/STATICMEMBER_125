aced1a101f98944aac3815e763912274c245a74b
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-05-15 17:34:28 +0700

    menambahkan nama Andi Kurniawan

--- STATICMEMBER_125/STATICMEMBER_125.cpp
+++ STATICMEMBER_125/STATICMEMBER_125.cpp
@@ -28,7 +28,7 @@ void mahasiswa::printALL() {
 int main() {
     mahasiswa mhs1("Aulia");
     mahasiswa mhs1("Asroni");
-    mahasiswa mhs1("Aulia");
+    mahasiswa mhs1("Andi Kurniawan");
     mahasiswa mhs1("Aulia");
 }
 
